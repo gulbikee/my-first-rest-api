d3dd4d1a9b9da8585afacd766def670a4fd7a380
Gülbike Aydın <66081842+gulbikee@users.noreply.github.com>
2022-08-29 09:43:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const cors=require('cors');
 const helmet = require('helmet');//farklı HTTP header'ları oluşturduk
 const morgan = require('morgan');
 
+
 const app=express();
 const ads = [
     {title: 'My first rest API attempt!'}
   ];
+  
+  
+  
   app.use(helmet());
   app.use(bodyParser.json);//json'ları ayrıştırıp js objelerine dönüştürdük
   app.use(cors());
